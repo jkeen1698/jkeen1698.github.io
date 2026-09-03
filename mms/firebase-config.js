7f5c0498293fb39b993eb2654192689d332a10cd
jkeen1698 <jkeen890@gmail.com>
2026-09-03 13:35:03 -0500

    Delete mms directory

--- mms/firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.FIN411_FIREBASE = {
-  apiKey: "AIzaSyArJx7i4cfpSGwDEFJrp4bS7T9yQcGbzjY",
-  authDomain: "fin411-cal.firebaseapp.com",
-  databaseURL: "https://fin411-cal-default-rtdb.firebaseio.com",
-  projectId: "fin411-cal",
-  storageBucket: "fin411-cal.firebasestorage.app",
-  messagingSenderId: "110938279308",
-  appId: "1:110938279308:web:c22ecad7bb933fb9d45aa9"
-};
